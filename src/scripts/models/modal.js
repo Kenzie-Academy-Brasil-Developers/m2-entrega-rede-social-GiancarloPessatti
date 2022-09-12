60b200660fd84c0d57d6c2c2cbcc3ac7210385f5
Giancarlo <giancarlopessatti@hotmail.com>
2022-09-12 04:08:40 -0300

    HTML/CSS Concept

---
+++ src/scripts/models/modal.js
@@ -0,0 +1,17 @@
+class Modal{
+
+    static alterandoModal (){
+
+        const buttonsControllersModal = document.querySelectorAll("[data-control-modal]")
+        for(let index = 0; index < buttonsControllersModal.length; index++){
+            buttonsControllersModal[index].addEventListener("click", ()=>{
+                let modalId = buttonsControllersModal[index].getAttribute("data-control-modal")
+                document.getElementById(modalId).classList.toggle("show-modal")
+            })
+        }
+
+    }
+
+}
+
+Modal.alterandoModal()
